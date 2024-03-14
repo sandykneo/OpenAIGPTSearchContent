dd73cd6d7c63cb6cec8f9992170f31e53f25c828
sandykneo <sandyk.neo@gmail.com>
2024-03-14 12:33:49 +0530

    Update azurecognitive_search_AzureOpenAI.py

--- LangChainAzureSearch/azurecognitive_search_AzureOpenAI.py
+++ LangChainAzureSearch/azurecognitive_search_AzureOpenAI.py
@@ -1,39 +1,35 @@
 import os
 import openai
 from os.path import join, dirname
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings,AzureOpenAIEmbeddings
 
 from langchain.vectorstores.azuresearch import AzureSearch
 from langchain_community.document_loaders import AzureBlobStorageContainerLoader
 from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
+
 #dotenv_path = join(dirname(__file__), '.env')
 #load_dotenv(dotenv_path)
 load_dotenv()
 
 openai.api_type = "azure"
-openai.api_base = os.getenv('AZURE_OPENAI_API_BASE')
+openai.api_base = os.getenv('AZURE_OPENAI_ENDPOINT')
 openai.api_key = os.getenv('AZURE_OPENAI_API_KEY')
-openai.api_version = os.getenv('AZURE_OPENAI_API_VERSION')
+openai.api_version = os.getenv('OPENAI_API_VERSION')
 
-model: str = os.environ.get('AZURE_OPENAI_EMBEDDING_MODEL_NAME')
+model: str = os.environ.get('OPENAI_DEPLOYMENT_NAME')
 #"text-embedding-3-large"
 
 vector_store_address: str = f"https://{os.environ.get('AZURE_COGNITIVE_SEARCH_SERVICE_NAME')}.search.windows.net"
 print(vector_store_address)
 
-
 #embeddings: OpenAIEmbeddings = OpenAIEmbeddings(deployment=model, chunk_size=1)
 
-
-embeddings = OpenAIEmbeddings(
-    openai_api_type="azure",
-    openai_api_key=os.getenv("OPENAI_API_KEY"),
-    openai_api_base=os.getenv("OPENAI_API_BASE"),
-    deployment=os.getenv("OPENAI_DEPLOYMENT_NAME"),
-    model=os.getenv("OPENAI_MODEL_NAME"),
-    chunk_size=1)
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment=os.getenv("OPENAI_DEPLOYMENT_NAME"),
+    openai_api_version=os.getenv("OPENAI_API_VERSION"),
+)
 
 index_name: str = "langchain-vector-demo"
 vector_store: AzureSearch = AzureSearch(
@@ -49,7 +45,7 @@ loader = AzureBlobStorageContainerLoader(
 )
 documents = loader.load()
 
-text_splitter = CharacterTextSplitter(chunk_size=1500, chunk_overlap=20)
+text_splitter = CharacterTextSplitter(chunk_size=4000, chunk_overlap=20)
 docs = text_splitter.split_documents(documents)
 vector_store.add_documents(documents=docs)
 
